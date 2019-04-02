fd9b86d62028edecd7ed0454278ecdad590e3c3c
Long Nguyen <longkt90@gmail.com>
2019-04-02 16:50:47 +0700

    add primary_endpoint_address to use in non-cluster

--- output.tf
+++ output.tf
@@ -17,3 +17,8 @@ output "host" {
   value       = "${module.dns.hostname}"
   description = "Redis host"
 }
+
+output "primary_endpoint_address" {
+  value       = "${aws_elasticache_replication_group.default.primary_endpoint_address}"
+  description = "The address of the endpoint for the primary node in the replication group, if the cluster mode is disabled."
+}
