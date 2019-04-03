7cb91bc4dd9e15ad13d43372a7c29aa09196b0f0
Long Nguyen <longkt90@gmail.com>
2019-04-03 08:42:19 +0700

    return list of the endpoints across all instances

--- output.tf
+++ output.tf
@@ -19,6 +19,6 @@ output "host" {
 }
 
 output "primary_endpoint_address" {
-  value       = "${aws_elasticache_replication_group.default.primary_endpoint_address}"
+  value       = "${aws_elasticache_replication_group.default.*.primary_endpoint_address}"
   description = "The address of the endpoint for the primary node in the replication group, if the cluster mode is disabled."
 }
